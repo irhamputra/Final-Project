33d01f2a55f6a250c81def1cd2681708f370e19e
irhamputra <irhamputraprasetyo@gmail.com>
2017-02-14 15:49:54 +0100

    Database updated: 14.02.2017

--- finalproject.sql
+++ finalproject.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Feb 04, 2017 at 01:34 AM
+-- Generation Time: Feb 14, 2017 at 03:48 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `finalproject`
 --
+CREATE DATABASE IF NOT EXISTS `finalproject` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `finalproject`;
 
 -- --------------------------------------------------------
 
@@ -26,8 +28,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `news`
 --
 
+DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
-  `news_id` int(11) NOT NULL,
   `news_title` varchar(320) NOT NULL,
   `news_description` text NOT NULL,
   `news_image_url` varchar(6000) NOT NULL,
@@ -39,8 +41,10 @@ CREATE TABLE `news` (
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`news_id`, `news_title`, `news_description`, `news_image_url`, `news_deadline`, `news_timestamp`) VALUES
-(0, 'Logo Design and Business Card Design', 'My company need logo & Business.\r\nso if you are challenged to make this for us, you can send your work to:\r\ninfo@blablaland.de with subject Logo Design & Business Card.\r\nPrice: 2000€ Cash\r\nGood luck guys!', 'blablaland.png', '15 June 2017', '2017-02-04 00:17:27');
+INSERT INTO `news` (`news_title`, `news_description`, `news_image_url`, `news_deadline`, `news_timestamp`) VALUES
+('Logo Design and Business Card Design', 'My company need logo & Business.\r\nso if you are challenged to make this for us, you can send your work to:\r\ninfo@blablaland.de with subject Logo Design & Business Card.\r\nPrice: 2000€ Cash\r\nGood luck guys!', 'blablaland.png', '15 June 2017', '2017-02-04 00:17:27'),
+('Web Design & Mobile App Design', 'We are the new startup based in Leipzig. We just need a couple amazing thing.\r\nAs you can see we need a great web design and mobile app design.\r\nWe have the budget only 1200€ in transfer.\r\nThis challenge is for everyone in Camping Art.\r\nSend us your artwork to hello@liebemachtspass.de with subject Web Design / Mobile App.\r\nGood Luck guys!', '', '12 August 2017', '2017-02-04 08:53:01'),
+('Brand design', 'Hey, we are form Vittel AG and we based in Berlin.\r\nI heard some many friends of mine, all people in here are absolute amazing.\r\nWe need you to help us to make our brand new design.\r\nIt could be for all challenger in Camping Art.\r\nWe have 3200€ for the winner and this project should be original and look fancy as well.\r\nAfter we got all design, we will call you soon as possible.\r\nWhat are you waiting for? Good luck', 'http://www.nestle-waters.com/asset-library/publishingimages/newfolder/imagebank/bouteilles/vittel_hr.png', '30 June 2017', '2017-02-13 11:15:08');
 
 -- --------------------------------------------------------
 
@@ -48,6 +52,7 @@ INSERT INTO `news` (`news_id`, `news_title`, `news_description`, `news_image_url
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `user_username` varchar(64) NOT NULL,
@@ -62,8 +67,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_username`, `user_password`, `user_email`, `user_role`, `user_created`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'whatever@gmail.com', 'admin', '2017-02-03 22:57:05'),
-(2, 'john_doe', '6579e96f76baa00787a28653876c6127', 'john_doe@gmail.com', 'client', '2017-02-03 22:57:05');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'irhamputraprasetyo@gmail.com', 'admin', '2017-02-10 19:20:27'),
+(0, 'irhamputra', 'ae51f075e63bfa905c95ee1c5f6fb036', 'hello@irhamputra.com', 'Designer', '2017-02-10 19:24:14'),
+(0, 'ulfapus', 'a08151cf65d440fc13ef28c94e0be3d8', 'ulfapus@gmail.com', 'Client', '2017-02-10 19:25:10');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
